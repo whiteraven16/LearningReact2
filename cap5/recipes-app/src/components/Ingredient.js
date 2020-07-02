b4ae648dec4ef9506bfa15af2298826ec8f44938
Elio Ruiz <elioruiz.en@gmail.com>
2020-07-01 22:07:26 -0400

    Fin del capitulo 5 aparentemente tengo que montar esos archivos en un servidor node para ver lo que hice pero dejo el codigo porque es util

---
+++ cap5/recipes-app/src/components/Ingredient.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Ingredient({amount,measurement,name})
+{
+    return
+    (
+        <li>
+            {amount} {measurement} {name}
+        </li>
+    );
+}
